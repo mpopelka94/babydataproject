05f3b630859df81abfe79a517ecf47b320eb6ac6
mpopelka94 <mpopelka@unomaha.edu>
2024-12-14 20:22:39 -0600

    added home HTML page, CSS styling, Javascript styling for graphs, and models for baby event data

--- babyapp/models.py
+++ babyapp/models.py
@@ -7,8 +7,16 @@ class Baby(models.Model):
     age = models.IntegerField()
     # Baby Allergies
     allergy = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at_time = models.DateTimeField(auto_now_add=True)
+    created_at_date = models.DateField(auto_now_add=True)
+
+class BabyEvent(Baby):
+    diaper_change = models.BooleanField(default=False)
+    dirty_diaper = models.BooleanField(default=False)
+    meal = models.CharField(max_length=100)
+    sleep = models.DurationField()
+
 
     def __str__(self):
         # return self.name
-        return f"Data: {self.name, self.age, self.allergy} (Added at: {self.created_at})"
+        return f"Data: {self.name, self.dirty_diaper, self.diaper_change, self.meal, self.sleep} (Added at: {self.created_at_date, self.created_at_time})"
