7bc64af82914ffb7904218e7adee090bda1bc6bd
mpopelka94 <mpopelka@unomaha.edu>
2024-12-16 11:11:37 -0600

    feature changes, sort entries ascending order

--- babyapp/urls.py
+++ babyapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('newdata/', views.input_data, name='input_data'),
     path('view/', views.view_data, name='view_data'),
+
+    path('accounts/login/', include('django.contrib.auth.urls')),
 
     path('accounts/', include('django.contrib.auth.urls')), # For authentication views
     path('register/', views.register, name='register'),
