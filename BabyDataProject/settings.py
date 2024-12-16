4d3626eddfa4195d9cd7f6522f6c8f965bd27a2c
mpopelka94 <mpopelka@unomaha.edu>
2024-12-16 16:13:08 -0600

    added Docker files to spin up app via Docker

--- BabyDataProject/settings.py
+++ BabyDataProject/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4fam_7)8m4=ts241vy24!=bs*u7ock_-aqt=8#teh*yi2#!!)w'
-
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY='django-insecure-4fam_7)8m4=ts241vy24!=bs*u7ock_-aqt=8#teh*yi2#!!)w'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG= os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '0.0.0.0'
+]
+# Passing ALLOWED_HOSTS as env variable instead of hardcoded for security
+# ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split(',')
 
 
 # Application definition
@@ -74,10 +83,31 @@ WSGI_APPLICATION = 'BabyDataProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'babydataapp',
+#         'USER': 'admin',
+#         'PASSWORD': 'admin12345',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', 'babydataapp'),
+        'USER': 'admin',
+        'PASSWORD': 'admin12345',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
@@ -116,8 +146,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
